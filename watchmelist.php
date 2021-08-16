5568dec0abc0502246a6672483d940f007002321
ASBHYCEPE <ashby.cepe@unc.edu.ph>
2021-08-16 14:56:58 +0800

    Added header and its css styling

---
+++ watchmelist.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>WATCHME LIST</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="main.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    </head>
+    <body>
+        <header>
+            <div id="title-wrapper">
+                <h1>WATCHME <span style="color: #27ae60">LIST</span></h1>
+            </div>
+        </header>
+    </body>
+</html>
