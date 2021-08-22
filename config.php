bd63d8233bb8d4f6f7d377408e8daa60ec14d692
ASBHYCEPE <ashby.cepe@unc.edu.ph>
2021-08-22 14:58:58 +0800

    Fixed credentials for MYSQL database connection

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
     define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'localhost');
+    define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
     define('DB_NAME', 'master');
 
     $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
     if($link == false){
-        die("ERROR COULD NOT CONNECT"..mysqli_connect_error());
+        die("ERROR COULD NOT CONNECT".mysqli_connect_error());
     }
 ?>
