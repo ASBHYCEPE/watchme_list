31c92f8e3b693df1f63139df312b4d53a1d0a110
ASBHYCEPE <ashby.cepe@unc.edu.ph>
2021-08-29 11:58:29 +0800

    Modified delete statement

--- clearHistory.php
+++ clearHistory.php
@@ -1,7 +1,7 @@
 <?php
     require "config.php";
 
-    $sql = "DELETE FROM watchlist WHERE show_status = 2 AND show_status = 3";
+    $sql = "DELETE FROM watchlist WHERE show_status > 1";
     
     if($stmt = mysqli_prepare($link, $sql)){
         mysqli_execute($stmt);
