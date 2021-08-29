379af3354ba4ede069c318f99b37089c4afadc12
ASBHYCEPE <ashby.cepe@unc.edu.ph>
2021-08-29 11:44:01 +0800

    Added another biniding parameter for update statement

--- updateShowStats.php
+++ updateShowStats.php
@@ -2,10 +2,11 @@
     require "config.php";
 
     $showID = $_GET['showID'];
-    $sql = "UPDATE watchlist SET show_status = 1 WHERE show_id = ?";
+    $showStats = $_GET['showStats'];
+    $sql = "UPDATE watchlist SET show_status = ? WHERE show_id = ?";
     
     if($stmt = mysqli_prepare($link, $sql)){
-        mysqli_stmt_bind_param($stmt, "s", $showID);
+        mysqli_stmt_bind_param($stmt, "is", $showStats, $showID);
         mysqli_execute($stmt);
         mysqli_stmt_close($stmt);
         header("location:watchmelist.php");
