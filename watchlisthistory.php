698a4c877febcc81b937737f31172734b4b53b13
ASBHYCEPE <ashby.cepe@unc.edu.ph>
2021-08-29 16:24:26 +0800

    Added another webpage for displaying watched or dropped shows

---
+++ watchlisthistory.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>WATCHME LIST</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="main.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Arimo&display=swap" rel="stylesheet">
+    </head>
+    <body>
+    <header>
+            <div id="title-wrapper">
+                <div id="logo">
+                    <div class="logo-item">
+                        <img src="img/logo.png" id="img-logo" alt="eye logo">
+                    </div>
+                    <div class="logo-item">
+                        <h1>WATCHME <span style="color: #27ae60">LIST</span></h1>
+                    </div>
+                </div>
+                <div id="nav">
+                    <a href="watchmelist.php">HOME</a>
+                    <a href="#">WATCHLIST HISTORY</a>
+                </div>
+            </div>
+        </header>
+        <div id="container">
+            <div id="category-container">
+                <div class="category-block">
+                    <div class="category-title">
+                        <h2>DROPPED SHOWS</h2>
+                    </div>
+                </div>
+                <div class="category-block">
+                    <div class="category-title">
+                        <h2>FINISHED WATCHING</h2>
+                    </div>
+                </div>
+            </div>
+    </body>
+</html>
